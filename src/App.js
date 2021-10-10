7cdbcc29c9ed3e602f7774a9b40bcd14177732df
arkdich <artem002498@gmail.com>
2021-10-10 15:46:07 +0600

    add wrapper transition effect

--- src/App.js
+++ src/App.js
@@ -3,32 +3,24 @@ import Header from './components/header/Header';
 import TaskForm from './components/taskForm/TaskForm';
 import TaskWrapper from './components/tasks/TaskWrapper';
 import Footer from './components/footer/Footer';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { tasksThunk } from './store/tasksSlice';
+import ReactDOM from 'react-dom';
 import './App.scss';
 
 export default function App() {
-  const isLoaded = useSelector((state) => state.tasks.isLoaded);
-  const isEditing = useSelector((state) => state.tasks.isEditing);
-
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(tasksThunk.fetchTasks());
   }, [dispatch]);
 
-  const tasksContainer = isLoaded ? (
-    <TaskWrapper isEditing={isEditing} />
-  ) : (
-    <div className="loading-gif"></div>
-  );
-
   return (
     <Fragment>
       <Header />
       <TaskForm />
-      {tasksContainer}
-      <Footer />
+      <TaskWrapper />
+      {ReactDOM.createPortal(<Footer />, document.querySelector('.footer'))}
     </Fragment>
   );
 }
--- src/components/tasks/TaskWrapper.js
+++ src/components/tasks/TaskWrapper.js
@@ -1,31 +1,48 @@
 import TaskItem from './TaskItem';
-import PropTypes from 'prop-types';
 import './TaskWrapper.scss';
 import { useSelector } from 'react-redux';
+import { useEffect, useRef } from 'react';
 
-export default function TaskWrapper(props) {
+export default function TaskWrapper() {
   const tasks = useSelector((state) => state.tasks.items);
+  const isLoaded = useSelector((state) => state.tasks.isLoaded);
+  const isEditing = useSelector((state) => state.tasks.isEditing);
 
-  const classString = `task-wrapper ${
-    props.isEditing ? 'task-wrapper_editing' : ''
-  }`;
+  const classString = `task-wrapper ${isEditing ? 'task-wrapper_editing' : ''}`;
+
+  const tasksWrapper = useRef();
+
+  useEffect(() => {
+    const wrapper = tasksWrapper.current;
+
+    if (!wrapper) return;
+
+    const contentHeight = Array.from(wrapper.childNodes).reduce(
+      (acc, curr) => (acc += curr.scrollHeight),
+      0
+    );
+
+    const computedStyle = getComputedStyle(wrapper);
+
+    wrapper.style.height =
+      contentHeight +
+      Number.parseInt(computedStyle.paddingTop) +
+      Number.parseInt(computedStyle.paddingBottom) +
+      'px';
+  });
 
   return (
-    <ul className={classString}>
+    <ul ref={tasksWrapper} className={classString}>
+      {!isLoaded && <div className="loading-gif"></div>}
       {tasks.map((task) => (
         <TaskItem
           key={task.id}
           id={task.id}
           value={task.value}
           isDone={task.isDone}
-          isEditing={props.isEditing}
-          // onTaskUpdate={props.onItemUpdate}
+          isEditing={isEditing}
         />
       ))}
     </ul>
   );
 }
-
-TaskWrapper.propTypes = {
-  isEditing: PropTypes.bool,
-};
