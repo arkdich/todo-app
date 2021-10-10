d48ebc25a4760ec8bb7e7817d47335a6e3a4dc28
arkdich <artem002498@gmail.com>
2021-10-10 17:27:29 +0600

    add delete transition

--- src/components/tasks/TaskItem.js
+++ src/components/tasks/TaskItem.js
@@ -1,10 +1,14 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { tasksThunk } from '../../store/tasksSlice';
 import PropTypes from 'prop-types';
+import { Transition } from 'react-transition-group';
 
 export default function TaskItem(props) {
   const [isChecked, setIsChecked] = useState(props.isDone);
+  const [isShown, setIsShown] = useState(true);
+
+  const nodeRef = useRef();
 
   const dispatch = useDispatch();
 
@@ -15,26 +19,42 @@ export default function TaskItem(props) {
   };
 
   const taskDeleteHandler = () => {
-    dispatch(tasksThunk.deleteTask(props.id));
+    setIsShown(false);
   };
 
   return (
-    <li className="task-wrapper__item">
-      <input
-        type="checkbox"
-        checked={isChecked}
-        id={`task-item-${props.id}`}
-        className="task-wrapper__check"
-        onChange={taskUpdateHandler}
-      ></input>
-      <label htmlFor={`task-item-${props.id}`} className="task-wrapper__value">
-        {props.value}
-      </label>
-      <button
-        className="task-wrapper__delete"
-        onClick={taskDeleteHandler}
-      ></button>
-    </li>
+    <Transition
+      nodeRef={nodeRef}
+      in={isShown}
+      timeout={300}
+      onExited={() => dispatch(tasksThunk.deleteTask(props.id))}
+    >
+      {() => {
+        const deleteStyle = !isShown ? ' task-wrapper__item_deleting' : '';
+
+        return (
+          <li ref={nodeRef} className={'task-wrapper__item' + deleteStyle}>
+            <input
+              type="checkbox"
+              checked={isChecked}
+              id={`task-item-${props.id}`}
+              className="task-wrapper__check"
+              onChange={taskUpdateHandler}
+            ></input>
+            <label
+              htmlFor={`task-item-${props.id}`}
+              className="task-wrapper__value"
+            >
+              {props.value}
+            </label>
+            <button
+              className="task-wrapper__delete"
+              onClick={taskDeleteHandler}
+            ></button>
+          </li>
+        );
+      }}
+    </Transition>
   );
 }
 
